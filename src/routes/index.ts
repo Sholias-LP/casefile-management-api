936699cd2cf4959e94bac6dd745e66aceb43eb1c
giwajossy <giwajosef@gmail.com>
2022-04-10 10:05:47 +0100

    Polished root route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,8 +7,8 @@ const welcomeRoute = (req: Request, res: Response) => {
   res.status(200).send(
     { 
       status: {
-        statusCode: 200,
-        statusMessage: "Success"
+        code: 200,
+        message: "Success"
       },
       message: 'Welcome to the Casefile Management Syetem API'
     })
