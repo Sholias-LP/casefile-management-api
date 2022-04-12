104185c1dcc120dd409d25a615de3128e13d18f9
giwajossy <giwajosef@gmail.com>
2022-04-12 14:43:29 +0100

    Rename endpoint to add a casefile

--- src/routes/casefile.route.ts
+++ src/routes/casefile.route.ts
@@ -4,7 +4,7 @@ import Casefile from '../handlers/casefiles'
 const router = express.Router()
 
 
-router.post('/casefiles/new',  Casefile.addASchool);
+router.post('/casefiles/new',  Casefile.addACasefile);
 router.get('/casefiles/',  Casefile.getAllCasefiles);
 router.get('/casefiles/:id',  Casefile.getACasefile);
 router.put('/casefiles/:id',  Casefile.updateACasefile);
