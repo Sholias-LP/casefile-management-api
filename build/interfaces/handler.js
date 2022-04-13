6efb5dde2b984a566b8e54ef63820e6d264ce7ea
giwajossy <giwajosef@gmail.com>
2022-04-14 00:34:23 +0100

    Build update

---
+++ build/interfaces/handler.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BaseHandler = void 0;
+class BaseHandler {
+    stringify(obj) {
+        return JSON.stringify(obj);
+    }
+}
+exports.BaseHandler = BaseHandler;
---
+++ build/models/casefile.model.js
@@ -0,0 +1,27 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Casefile extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    Casefile.init({
+        caseID: DataTypes.STRING,
+        caseType: DataTypes.STRING,
+        client: DataTypes.STRING,
+        gender: DataTypes.STRING,
+        occupation: DataTypes.STRING,
+        brief: DataTypes.STRING,
+        letter_of_engagement: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'Casefile',
+    });
+    return Casefile;
+};
