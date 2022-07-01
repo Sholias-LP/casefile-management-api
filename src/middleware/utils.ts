1fca94ab6487781b33e23107408c695d6a2e3e43
giwajossy <giwajosef@gmail.com>
2022-07-01 17:28:49 +0100

    upadate CORS

--- src/middleware/utils.ts
+++ src/middleware/utils.ts
@@ -10,7 +10,7 @@ export const getToken = (req: Request) => {
 export const cors = (_req: Request, res: Response, next: NextFunction) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type,  Accept, Authorization');
   next();
 };
 
