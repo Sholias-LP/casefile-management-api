1a9bf29395297a4525051606f3ca3a7345c2ffe2
giwajossy <giwajosef@gmail.com>
2022-07-12 02:54:22 +0100

    light cleanup

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -72,7 +72,7 @@ class User {
 
                 if (user) {
                     const payload = {
-                        id: user._id,
+                        _id: user._id,
                         first_name: user.first_name,
                         last_name: user.last_name,
                         email: user.email,
